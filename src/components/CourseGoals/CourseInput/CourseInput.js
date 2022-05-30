7829cebe94d0360197dfbbe50ef9c03dc566b88d
zooyaho <jiwoo1268@naver.com>
2022-05-30 12:41:27 +0900

    inline스타일을 사용하여 빈값 전송시 스타일 변경되는 작업, 조건부로 특정 스타일을 설정함.

--- src/components/CourseGoals/CourseInput/CourseInput.js
+++ src/components/CourseGoals/CourseInput/CourseInput.js
@@ -1,25 +1,45 @@
-import React, { useState } from 'react';
+import React, { useState } from "react";
 
-import Button from '../../UI/Button/Button';
-import './CourseInput.css';
+import Button from "../../UI/Button/Button";
+import "./CourseInput.css";
 
-const CourseInput = props => {
-  const [enteredValue, setEnteredValue] = useState('');
+const CourseInput = (props) => {
+  const [enteredValue, setEnteredValue] = useState("");
+  // 입력값이 유효한지 체크하는 state
+  const [isValid, setIsValid] = useState(true);
 
-  const goalInputChangeHandler = event => {
+  const goalInputChangeHandler = (event) => {
+    if(event.target.value.trim().length > 0){
+      setIsValid(true);
+    }
     setEnteredValue(event.target.value);
   };
 
-  const formSubmitHandler = event => {
+  const formSubmitHandler = (event) => {
     event.preventDefault();
+
+    // 입력한 값이 아무것도 없을때 list에 추가하지 않음.
+    if (enteredValue.trim().length === 0) {
+      setIsValid(false);
+      return;
+    }
+
     props.onAddGoal(enteredValue);
   };
 
   return (
     <form onSubmit={formSubmitHandler}>
       <div className="form-control">
-        <label>Course Goal</label>
-        <input type="text" onChange={goalInputChangeHandler} />
+        {/* 인라인 스타일 props는 값으로 객체를 갖음. */}
+        <label style={{ color: !isValid ? "red" : "black" }}>Course Goal</label>
+        <input
+          type="text"
+          onChange={goalInputChangeHandler}
+          style={{
+            backgroundColor: !isValid ? "salmon" : "transparent",
+            borderColor: !isValid ? "red" : "#ccc",
+          }}
+        />
       </div>
       <Button type="submit">Add Goal</Button>
     </form>
